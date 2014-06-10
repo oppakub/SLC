9665d35069ebbbbbff0a89ea7362ea2f7e0a5db4
pagapong <ck_pasalmon@hotmail.com>
2014-06-10 23:05:07 +0700

    first use 1/2

--- app/src/main/java/app/contest/kmitl/slc/MainActivity.java
+++ app/src/main/java/app/contest/kmitl/slc/MainActivity.java
@@ -1,9 +1,11 @@
 package app.contest.kmitl.slc;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -14,6 +16,11 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void ClickFirstUse(View view){
+        Intent objFirstUse = new Intent(MainActivity.this, FirstUse.class);
+        startActivity(objFirstUse);
+        finish();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
