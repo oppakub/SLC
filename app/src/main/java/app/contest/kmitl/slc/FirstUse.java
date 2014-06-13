bf9cae62a2de5e9d61c8a53d6febe93595de6ac7
pagapong <ck_pasalmon@hotmail.com>
2014-06-13 12:50:25 +0700

    erer

--- app/src/main/java/app/contest/kmitl/slc/FirstUse.java
+++ app/src/main/java/app/contest/kmitl/slc/FirstUse.java
@@ -12,28 +12,28 @@ public class FirstUse extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+       // requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         setContentView(R.layout.activity_first_use);
-        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.customtitlebar);
+       // getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.customtitlebar);
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.first_use, menu);
-        return true;
-    }
+ //   @Override
+  //  public boolean onCreateOptionsMenu(Menu menu) {
+   //     // Inflate the menu; this adds items to the action bar if it is present.
+    //    getMenuInflater().inflate(R.menu.first_use, menu);
+     //   return true;
+  //  }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+ //   @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+ //       int id = item.getItemId();
+//        if (id == R.id.action_settings) {
+ //           return true;
+ //       }
+ //       return super.onOptionsItemSelected(item);
+   // }
 }
